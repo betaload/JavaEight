5179bf2a4be62041a00e1f1677b456b37aae1a8e
squallsun <squallsun@hotmail.com>
2015-02-04 08:10:58 +0800

    rename

--- src/main/java/com/squallsun/practices/Java8ReallyImpatientExcises/Chapter1.java
+++ src/main/java/com/squallsun/practices/Java8ReallyImpatientExcises/Chapter1.java
@@ -1,4 +1,4 @@
-package com.sunjq.practices.Java8ReallyImpatientExcises;
+package com.squallsun.practices.Java8ReallyImpatientExcises;
 
 import java.io.File;
 import java.io.FileFilter;
--- src/main/java/com/squallsun/practices/Java8ReallyImpatientExcises/Chapter2.java
+++ src/main/java/com/squallsun/practices/Java8ReallyImpatientExcises/Chapter2.java
@@ -1,4 +1,4 @@
-package com.sunjq.practices.Java8ReallyImpatientExcises;
+package com.squallsun.practices.Java8ReallyImpatientExcises;
 
 /**
  * Created by SJQ on 2015/2/1.
